bb4ed1a2087f59f9b6e63f743a016071bc8db352
Beepered <bktrieu@gmail.com>
2025-07-10 15:56:24 -0700

    SUPER VUE stuff

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
+https://Beepered.github.io/src/index.html
+
 # Beepered's Portfolio
 
-https://Beepered.github.io
 That's right! It's my portfolio. For porting with a little folio.
 Clicky clicky. AAAAAAHHHHH!!!
+
+## Who am I?
+
+I don't know?
+
+## Where am I?
+
+I ... I, I don't know
+
+## What am I?
+
+Stop! uhhhuhhhuhhhuhh. Stop asking me this stuff
+
+## Why am I?
+
+Chunky Cheese
+
+### Remember the left side of the oven stays on the left
+
+Why are you reading this? Just go look at my portfolio. Even though it is super unfinished.
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,33 @@
+<!--
+Progress Bar
+https://www.youtube.com/watch?v=-q8rtVK_Z04&t=558s&ab_channel=TylerPotts
+-->
+
+<template>
+  <div class="progress_bar">
+    <div class="progress_inner">
+      <span>0%</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "progress_bar",
+
+}
+</script>
+
+<style>
+.progress_bar {
+  width: 70%;
+  height: 15%;
+  background-color: #b4b4b4;
+  position: relative;
+}
+
+.progress_inner {
+  height: 100%;
+  background-image: linear-gradient(to right, #0ccc76, #055632);
+}
+</style>
